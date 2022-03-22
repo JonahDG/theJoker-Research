f547cb3b23faef33bd1cd30b8c25f398907f546a
Jonah Goldfine <jdg577@nyu.edu>
2022-03-22 18:47:44 -0400

    Removed Unnecessary Import

---
+++ Code/theJoker_Compare_Periodogram_EccenVsPeriod.py
@@ -0,0 +1,9 @@
+# Imports
+import astropy as ap
+import astropy.units as u
+import astropy.table as at
+import astropy.coordinates as coords
+from astropy.io import ascii
+from astropy.io import fits
+from astropy.time import Time
+from astropy.timeseries import LombScargle
