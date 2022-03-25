5df08cc700166f7c8edff661d392796083f9a659
Jonah Goldfine <jdg577@nyu.edu>
2022-03-25 14:30:26 -0400

    Remove Unused import

--- Code/theJoker_Compare_Periodogram_EccenVsPeriod.py
+++ Code/theJoker_Compare_Periodogram_EccenVsPeriod.py
@@ -14,7 +14,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import lightkurve as lk
-from sympy import source
 import thejoker as tj
 from fpdf import FPDF
 #endregion
